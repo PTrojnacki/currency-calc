457a3c066e6d59205e70171aabb0e4ec7a7a5a95
Paweł Trojnacki <p.trojnacki@o2.pl>
2019-08-09 16:17:30 +0200

    Updates

---
+++ precache-manifest.8d3f260034935c84fe4c143c61309c25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef608a1c672bfca9619755524c83f66",
+    "url": "/currency-calc/index.html"
+  },
+  {
+    "revision": "bb39e27cd2ade3e73f06",
+    "url": "/currency-calc/static/css/main.6eca7843.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/currency-calc/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "bb39e27cd2ade3e73f06",
+    "url": "/currency-calc/static/js/main.fe499553.chunk.js"
+  },
+  {
+    "revision": "51c3e48e3359d19efbfd",
+    "url": "/currency-calc/static/js/runtime~main.9aaf3154.js"
+  }
+]);
